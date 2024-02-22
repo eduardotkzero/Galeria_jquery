074333b41c4920c8c1c738af1cf59233cf9e2dfb
Eduardo Souza <eduardoeulalia3@gmail.com>
2024-02-22 18:16:42 -0300

    Primeiro commit

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-const form = document.getElementById('form-contato');
-let linhas = '';
-
- 
-
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const inputNome= document.getElementById ('input-nome')
-    const inputTelefone= document.getElementById ('input-telefone')
-    
-    
-    let linha = `<tr>`
-    linha += `<td>${inputNome.value}</td>`;
-    linha += `<td>${inputTelefone.value}</td>`;
-    linha += `</tr>`;
-    linhas += linha;
-    linhas += '';
-   
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-
-    
+$('header button').click(function() {
+    $('form').slideDown();
 })
+
+$('#botao-cancelar').click(function() {
+    $('form').slideUp()
+})
+
+
+    $('form').on('submit', function(e) {
+        e.preventDefault ()
+
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+       $(`
+       <div class="overlay-imagem-link">
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real"
+                Ver imagem em tamanho real
+            </a>
+       </div>
+       `).appendTo(novoItem);
+       $(novoItem).appendTo('ul');
+       $(novoItem).fadeIn();
+       $('#endereco-imagem-nova').val('')
+        
+    })
